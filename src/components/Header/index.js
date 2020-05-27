d8f6c27a5237d95878804aa1bb2df8cb12649c12
Augusto Carlos <augustocarlos9966@gmail.com>
2020-05-27 01:13:21 +0100

    Adicionando o botão do menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { MdAccountCircle } from 'react-icons/md';
+import Search from '../Search';
+
+import { MdAccountCircle, MdExpandMore, MdMenu } from 'react-icons/md';
 import './styles.css';
 
 const Header = () => {
@@ -10,12 +12,22 @@ const Header = () => {
         id: localStorage.getItem('USER_ID')
     }
 
+    function handleSideNav(){
+        const container = document.querySelector('.grid-container')
+        console.log(container)
+        container.classList.toggle('w-sidenav')
+    }
+
     return (
         <header className="header">
+            <MdMenu size={25} onClick={handleSideNav}/>
+
+            <Search placeholder="buscar por posts..." />
 
                 <div className="login">
                     <MdAccountCircle size={24} />
                     <span>{user ? user.name : 'nome do usuário'}</span>
+                <MdExpandMore size={18} />
                 </div>
 
         </header>
